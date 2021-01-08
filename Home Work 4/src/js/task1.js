bba1b831c9764749ab0e51d2721c0c848eca6cbf
Anton-Mosquito <anton0886@outlook.com>
2021-01-08 13:16:20 +0200

    fix first task

--- Home Work 4/src/js/task1.js
+++ Home Work 4/src/js/task1.js
@@ -1,5 +1,12 @@
-function perform(arg, func) {
-  return Promise.resolve(func(arg));
+function perform(...args1) {
+  let func1 = args1.pop();
+  let result = func1.apply(this, args1);
+  return {
+    then: function (...args2) {
+      let func2 = args2.pop();
+      return perform.apply(this, [...args2, result, func2]);
+    },
+  };
 }
 
 perform(20, function (value) {
@@ -16,3 +23,68 @@ perform(20, function (value) {
     console.log(++param);
     return param;
   });
+
+/////// My Promise
+const moop = () => {};
+class MyPromise {
+  constructor(executor) {
+    this.queue = [];
+    this.errorHandler = moop;
+    this.finnalyHndler = moop;
+
+    try {
+      executor.call(null, this.onResolve.bind(this), this.onReject.bind(this));
+    } catch (e) {
+      this.errorHandler(e);
+    } finally {
+      this.finnalyHndler();
+    }
+  }
+
+  onResolve(data) {
+    this.queue.forEach((callback) => {
+      data = callback(data);
+    });
+
+    this.finnalyHndler();
+  }
+
+  onReject(error) {
+    this.errorHandler(error);
+    this.finnalyHndler();
+  }
+
+  then(fn) {
+    this.queue.push(fn);
+    return this;
+  }
+
+  catch(fn) {
+    this.errorHandler = fn;
+    return this;
+  }
+
+  finally(fn) {
+    this.finnalyHndler = fn;
+    return this;
+  }
+}
+
+const myPromise = new MyPromise();
+
+const promise = new MyPromise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("NgRx");
+    // reject("Some Error");
+  }, 150);
+});
+
+promise
+  .then((course) => {
+    console.log("Promise ", course.toUpperCase());
+  })
+  .then((title) => console.log(title))
+  .catch((error) => {
+    console.log("Error", error);
+  })
+  .finally(() => console.log("finnaly"));
